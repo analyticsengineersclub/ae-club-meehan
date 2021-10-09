d98360ca555b8c45233c85d6639739fb3220f561
datadisciple <sean@sparkgrills.com>
2021-10-09 06:08:01 +0000

    refactors query based off week 0 teachings

--- customers.sql
+++ customers.sql
@@ -1,13 +1,21 @@
+with customer_orders as (
+    select
+        customer_id,
+        count(distinct orders.id) as number_of_orders,
+        min(created_at) as first_order_at
+    from `analytics-engineers-club.coffee_shop.orders` orders
+    group by customer_id
+)
+
 select
-    c.id as customer_id,
-    max(c.name) as name,
-    max(c.email) as email,
-    min(o.created_at) as first_order_at,
-    count(*) as number_of_orders
+    customers.id as customer_id,
+    customers.name,
+    customers.email,
+    customer_orders.first_order_at,
+    customer_orders.number_of_orders
 
-from `analytics-engineers-club.coffee_shop.customers` c
-    join `analytics-engineers-club.coffee_shop.orders` o on c.id = o.customer_id
+from `analytics-engineers-club.coffee_shop.customers` customers
+    left join customer_orders on customers.id = customer_orders.customer_id
 
-group by c.id
-order by first_order_at
+order by first_order_at asc
 limit 5
